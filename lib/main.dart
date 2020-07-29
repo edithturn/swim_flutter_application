7230efd40ae83fa3583b771b4ec88c4895a338dd
alineayumi <alineayumi@gmail.com>
2020-07-29 20:40:49 -0300

    full main

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-<<<<<<< HEAD
-<<<<<<< HEAD
   home: Scaffold(
     appBar: AppBar(
       title: Text('my first app'),
       centerTitle: true,
     )
-=======
-  body: Center(
-    child: Text('Edith Part!'),
-  ),
->>>>>>> cd66f3ab4411bc46b7f2bd7fbb4f83f13ecc9ef3
-=======
-
-  home: Text('Hello girls!'),
   body: Center(
     child: Text('Edith Part!'),
   ),
   floatingActionButton: FloatingActionButton(
-    child:Text('click'),
->>>>>>> 6de29908a9508f9f7c90e04f742c7b3be000a080
+    child:Text('click')
+  ),
 ));
 
