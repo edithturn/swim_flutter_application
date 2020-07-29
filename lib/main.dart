17b5c10d38d92dd1705eb97b8177ec63553a2cff
piratelicorne <thecodeloops@gmail.com>
2020-07-29 18:33:57 -0500

    floating button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('Hello girls!')
+  home: Text('Hello girls!'),
+  floatingActionButton: FloatingActionButton(
+    child:Text('click'),
+  ),
 ));
 
