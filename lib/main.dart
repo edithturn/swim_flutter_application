b270751e48088ac1e98b09dcb234a9fd2536d1ba
piratelicorne <thecodeloops@gmail.com>
2020-07-31 13:12:05 -0500

    testing with body:column

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 
+/*
+  body: Column (),
+  used to add a list of answer options
+*/
+
 void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(
       title: Text('my first app'),
       centerTitle: true,
-    )
-  body: Center(
-    child: Text('Edith Part!'),
-  ),
-  floatingActionButton: FloatingActionButton(
-    child:Text('click')
+    ),
+    body: Column(
+      children: [
+        Text('Questions:'),
+        RaisedButton(child: Text('Answer 1'), onPressed: null,),
+        RaisedButton(child: Text('Answer 2'), onPressed: null,),
+      ],
+    ),
+    floatingActionButton: FloatingActionButton(
+      child:Text('click here'),
+    ),
   ),
 ));
-
+ 
